0d5e4cc6629a757ead34f570951bd7e6f0b0e7a5
Alisher12 <@Alixan12@example.com>
2024-09-27 19:27:54 +0500

    first commit

---
+++ functions/api.mjs
@@ -0,0 +1,18 @@
+import express from "express";
+import serverless from "serverless-http"
+
+const app = express();
+
+app.get("/" , (req,res) => {
+    res.send('Serverdan salom!')
+});
+
+app.get('/abaut', (req,res) => {
+    res.json({
+        name : 'Alisher',
+        surname : 'Qurbonov',
+        age : 21
+    });
+});
+
+export const handler = serverless(app); 
